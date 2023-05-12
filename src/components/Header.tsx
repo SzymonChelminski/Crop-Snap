c6c2cc76a22af9a64e9a32db0e1830bc2e0e428f
Szymon Chełmiński <Szymon>
2023-05-12 22:58:59 +0200

    Header & Description works

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import logo from '../images/logo.png'
 export default function Header() {
   return (
     <div className='header'>
-        <div className='navbar'>
+        <nav className='navbar'>
           <div className='navbar__elements navbar__brand'>
             <img src={logo} alt="logo" className='navbar__logo'/>
             <span className='navbar__name'>
@@ -23,6 +23,17 @@ export default function Header() {
                 register
               </span>
           </div>
+        </nav>
+        <div className='content__container'>
+          <section className='text'>
+            <h1 className='text__header'>
+              Looking to improve quality of your images without much effort?
+            </h1>
+            <p className='text__description'>
+              Welcome to Crop&Snap, a place where perfection meets with ease. Are you just searching for a quick photo-editing site? Your friends recommended it, or mayby u stumbled upon this page randomly?
+              No worries, with Crop&Snap you can explore the world of photography from different view, we up here like to do things our way, make sad photos come alive or do some minor enhancements! 
+            </p>
+          </section>
         </div>
     </div>
   )
