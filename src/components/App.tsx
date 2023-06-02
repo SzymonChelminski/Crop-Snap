75d71a5a0713dee108c8ac939ac1b21f0c514595
Szymon Chełmiński <Szymon>
2023-06-02 22:50:37 +0200

    Logo animation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,31 @@ import React from 'react'
 
 // Components
 import Header from './Header'
+import ContentFirst from './ContentFirst'
+
+//Content animattion - scroll/fade in
+
+function obsever() {
+  let observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('show')
+      }
+      else{
+        entry.target.classList.remove('show')
+      }
+    })
+  })
+  
+  const hiddenElement = Array.from(document.getElementsByClassName('hidden') as HTMLCollectionOf<HTMLElement>)
+  observer.observe(hiddenElement[0])
+}
 
 export default function App() {
   return (
-    <div className='app'>
+    <div className='app' onLoad={obsever}>
       <Header />
+      <ContentFirst />
     </div>
   )
 }
---
+++ src/components/ContentFirst.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+//Images
+import crop_content_fris from '../images/crop_content_frist.png'
+
+export default function ContentFirst() {
+  return (
+      <div className='content__container'>
+        <div className='content__first hidden'>
+          <section className='content__first__text'>
+            <h1 className='content__first__header'>
+              <span>
+              <span className='content__first__header__sparkles'>Crop images </span> 
+              </span>
+              in few seconds!
+            </h1>
+            <span className='content__first__description'>
+              Picture can't fit or overflows the frame?
+              Quickly adjust size and proportions of your favourite photos with our <a href="squaretemplates__container" className='description__link'> free to use tools.</a>
+            </span>
+          </section>
+          <div className='content__first__img'>
+            <img src={crop_content_fris} alt="cropimg" />
+          </div>
+        </div>
+      </div>
+  )
+}
