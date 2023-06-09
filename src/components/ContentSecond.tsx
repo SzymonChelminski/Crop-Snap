f1cd8c87cfcaf43c2fc4cf3cddc555c41174352d
Szymon Chełmiński <Szymon>
2023-06-09 21:56:06 +0200

    carousel finishing touches

--- src/components/ContentSecond.tsx
+++ src/components/ContentSecond.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // images
 import rain from '../images/rain.png'
@@ -8,11 +8,40 @@ import saturated from '../images/saturated.png'
 import sun from '../images/sun.svg'
 import wind from '../images/wind.svg'
 
+
 export default function ContentSecond() {
+
+// arrows 
+const [leap, setLeap] = useState(0)
+
+useEffect(() => {
+  const pathElements = [...document.querySelectorAll<HTMLElement>('.carousel__element')]
+
+  pathElements.forEach(element =>{
+    element.style.right = `${leap}px`
+  })
+},[leap])
+
   return (
     <div className='content__secound__outline'>
         <div className='content__container'>
             <div className='content__second'>
+                  <span className='content__second__arrows' onClick={(e) => {
+
+                    const clickedElement = e.target as HTMLDivElement;
+                    const pathElementLength = document.querySelector<HTMLElement>('.carousel__element')!.offsetWidth
+
+                    if(clickedElement.className === 'fa-solid fa-chevron-left' && leap > 0) {
+                      setLeap(prevLeap => prevLeap - (pathElementLength + 20))
+                    }
+                    else if(clickedElement.className === 'fa-solid fa-chevron-right' && leap <= 549) {
+                      setLeap(prevLeap => prevLeap + (pathElementLength + 20))
+                    }
+    
+                  }}>
+                    <i className="fa-solid fa-chevron-left" ></i>
+                    <i className="fa-solid fa-chevron-right" ></i>
+                  </span>
                     <div className='carousel__element'>
                       <section className='carousel__img'>
                         <img src={sun} alt=""/>
@@ -70,7 +99,7 @@ export default function ContentSecond() {
                     Get into themes with <span className='content__first__header__sparkles'>templates</span>!
                     </h1>
                    <span className='content__second__description'>
-                      Research shows the best parties are the ones with theme clothing. We at Crop&Snap belive the same sticks with images.
+                      Research shows the best parties are the ones with theme clothing. We at Crop&Snap belive the same sticks to images.
                       <span className='slide__info'>
                           (Scroll to see available<span className='content__second__sparkles'>Themes</span>)
                       </span>
